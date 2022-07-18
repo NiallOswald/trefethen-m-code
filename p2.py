5ee6c1801578c07ad3561827ad20a40020c6b621
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-07-18 21:25:45 +0100

    Convert p2 into Python

---
+++ p2.py
@@ -0,0 +1,27 @@
+import numpy as np
+import scipy.linalg as sp
+import matplotlib.pyplot as plt
+
+for n in range(2, 101, 2):
+    h = 2 * np.pi / n
+    x = np.arange(1, n + 1) * h - np.pi
+    u = np.exp(np.sin(x))
+    du = np.cos(x) * u
+
+    # Construct the spectral differentiation matrix
+    column = np.concatenate(
+        [[0], 0.5 * (-1) ** np.arange(1, n) / np.tan(np.arange(1, n) * h / 2)]
+    )
+    index = np.concatenate([[0], np.arange(n - 1, 0, -1)])
+    D = sp.toeplitz(column, column[index])
+
+    # Compute errors
+    error = np.linalg.norm(D @ u - du, np.inf)
+
+    plt.loglog(n, error, "k.", markersize=8)
+
+plt.xlabel("N")
+plt.ylabel("error")
+plt.title("Convergence of spectral differentiation")
+plt.grid(which="both")
+plt.show()
