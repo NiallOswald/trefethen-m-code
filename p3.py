f770d4ef5b11356ff5cc74cfd89d73e069d5ef79
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-07-20 15:17:44 +0100

    Convert p3.m into Python

---
+++ p3.py
@@ -0,0 +1,34 @@
+"""p3 - Band-limited interpolation"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, axs = plt.subplots(3, 1, sharex=True)
+
+h = 1
+x_max = 10
+x = np.arange(-x_max, x_max + h, h)
+xx = np.arange(-x_max - h / 20, x_max + 3 * h / 20, h / 10)
+
+for i in range(3):
+    if i == 0:
+        v = x == 0
+    elif i == 1:
+        v = abs(x) <= 3
+    elif i == 2:
+        v = np.maximum(np.zeros(len(x)), 1 - abs(x) / 3)
+
+    axs[i].plot(x, v, "k.", markersize=8)
+
+    p = np.zeros(len(xx))
+    for j in range(len(x)):
+        p += v[j] * np.sin(np.pi * (xx - x[j]) / h) / (np.pi * (xx - x[j]) / h)
+
+    axs[i].plot(xx, p, "k", linewidth=0.75)
+    axs[i].set_xlim(-x_max, x_max)
+    axs[i].set_ylim(-0.5, 1.5)
+    axs[i].set_xticks([])
+    axs[i].set_yticks([0, 1])
+    axs[i].grid(which="both")
+
+plt.show()
