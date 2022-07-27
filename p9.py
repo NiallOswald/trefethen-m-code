ff37cf19496a687a80b1f187181ce6b90d764fec
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-07-27 13:56:11 +0100

    Convert p9.m into Python

---
+++ p9.py
@@ -0,0 +1,37 @@
+# p9 - polynomial interpolation in equispaced and Chebyshev points
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 16
+xx = np.arange(-1.01, 1.015, 0.005)
+
+plt.figure(figsize=(7, 3))
+
+for i in range(2):
+    if not i:
+        s = "equispaced points"
+        x = -1 + 2 * np.arange(n + 1) / n
+    else:
+        s = "Chebyshev points"
+        x = np.cos(np.pi * np.arange(n + 1) / n)
+
+    plt.subplot(1, 2, i + 1)
+
+    u = 1 / (1 + 16 * x**2)
+    uu = 1 / (1 + 16 * xx**2)
+    p = np.polyfit(x, u, n)
+    pp = np.polyval(p, xx)
+
+    plt.plot(x, u, "k.", markersize=8)
+    plt.plot(xx, pp, "k-", linewidth=0.75)
+
+    plt.xlim(-1.1, 1.1)
+    plt.ylim(-1, 1.5)
+    plt.title(s)
+
+    error = np.linalg.norm(uu - pp, np.inf)
+    plt.text(-0.5, -0.5, f"max error = {error:.5g}")
+
+plt.tight_layout()
+plt.show()
