62b0b4dfa90127273e0a744169918b9e4975cb12
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-08-02 17:02:13 +0100

    Add docstring to cheb

--- cheb.py
+++ cheb.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 def cheb(n):
+    """Return the Chebyshev differentiation matrix of order n."""
     if n == 0:
         return 0, 1
 
@@ -11,9 +12,9 @@ def cheb(n):
     c = np.array(
         [np.concatenate([[2], np.ones(n - 1), [2]]) * (-1) ** np.arange(n + 1)]
     ).T
-    X = np.tile(x, (1, n + 1))
-    dX = X - X.T
-    D = (c @ (1 / c).T) / (dX + np.eye(n + 1))
-    D = D - np.diag(np.sum(D.T, axis=0))
+    X = np.tile(x, (1, n + 1))  # noqa: N806
+    dX = X - X.T  # noqa: N806
+    D = (c @ (1 / c).T) / (dX + np.eye(n + 1))  # noqa: N806
+    D = D - np.diag(np.sum(D.T, axis=0))  # noqa: N806
 
     return D, x.T[0]
